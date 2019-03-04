142de534804b5e5d364cabc74f973b513d079a16
Blake <blakeaw1102@gmail.com>
2019-03-04 00:33:26 -0600

    New stopping criterion

--- gleipnir/stopping_criterion.py
+++ gleipnir/stopping_criterion.py
@@ -8,3 +8,12 @@ class NumberOfIterations(object):
 
     def __call__(self, nested_sampler):
         return nested_sampler._n_iterations >= self.n_iterations
+
+class RemainingPriorMass(object):
+
+    def __init__(self, cutoff):
+        self.cutoff = cutoff
+        return
+
+    def __call__(self, nested_sampler):
+        return nested_sampler._alpha**nested_sampler._n_iterations <= self.cutoff
