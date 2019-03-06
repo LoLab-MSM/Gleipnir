abc7979545f5f737607a3b669f795e4c2dabdb93
Blake <blakeaw1102@gmail.com>
2019-03-05 18:42:34 -0600

    Added the eggbox example.

---
+++ examples/eggbox/NS_eggbox_polychord.py
@@ -0,0 +1,42 @@
+"""
+Implementation of the 2-dimensional eggbox problem adapted from the
+pymultinest_demo.py at:
+https://github.com/JohannesBuchner/PyMultiNest/blob/master/pymultinest_demo.py
+"""
+import numpy as np
+from numpy import exp, log, pi
+from scipy.stats import uniform
+import matplotlib.pyplot as plt
+from gleipnir.sampled_parameter import SampledParameter
+from gleipnir.polychord import PolyChordNestedSampling
+
+
+
+# Number of paramters to sample is 1
+ndim = 2
+# Set up the list of sampled parameters: the prior is Uniform(0:10*pi) --
+# we are using a fixed uniform prior from scipy.stats
+sampled_parameters = [SampledParameter(name=i, prior=uniform(loc=0.0,scale=10.0*np.pi)) for i in range(ndim)]
+
+# Define the loglikelihood function
+def loglikelihood(sampled_parameter_vector):
+    chi = (np.cos(sampled_parameter_vector)).prod()
+    return (2. + chi)**5
+
+population_size = 100
+
+# Setup the Nested Sampling run
+n_params = len(sampled_parameters)
+print("Sampling a total of {} parameters".format(n_params))
+#population_size = 10
+print("Will use NS population size of {}".format(population_size))
+# Construct the Nested Sampler
+PCNS = PolyChordNestedSampling(sampled_parameters=sampled_parameters,
+                               loglikelihood=loglikelihood,
+                               population_size=population_size)
+#print(PCNS.likelihood(np.array([1.0])))
+#quit()
+# run it
+PCNS.run()
+# Print the output -- logZ should be approximately 236
+print(PCNS.output)
