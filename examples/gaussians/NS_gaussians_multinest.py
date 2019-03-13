bdebfe9d09f8dc5b4bb142e1ad65a85302fc5b0a
Blake <blakeaw1102@gmail.com>
2019-03-13 18:43:41 -0500

    Implementations of the 5-d gaussian example from DNest4's python examples.

---
+++ examples/gaussians/NS_gaussians_multinest.py
@@ -0,0 +1,68 @@
+"""
+Implementation of the 5-dimensional gaussian example adapted from the
+the DNest4 python examples:
+https://github.com/eggplantbren/DNest4/blob/master/python/examples/gaussian/gaussian.py
+"""
+import numpy as np
+from numpy import exp, log, pi
+from scipy.stats import uniform
+from scipy.special import erf
+import matplotlib.pyplot as plt
+from gleipnir.sampled_parameter import SampledParameter
+from gleipnir.multinest import MultiNestNestedSampling
+
+
+
+# Number of paramters to sample is 5
+ndim = 5
+
+# Define the loglikelihood function
+def loglikelihood(sampled_parameter_vector):
+    const = -0.5*np.log(2*np.pi)
+    return -0.5*np.sum(sampled_parameter_vector**2) + ndim * const
+width = 10.0
+def analytic_log_evidence(ndim, width):
+      lZ = (ndim * np.log(erf(0.5*width/np.sqrt(2)))) - (ndim * np.log(width))
+      return lZ
+
+if __name__ == '__main__':
+
+    # Set up the list of sampled parameters: the prior is Uniform(-5:5) --
+    # we are using a fixed uniform prior from scipy.stats
+    sampled_parameters = [SampledParameter(name=i, prior=uniform(loc=-5.0,scale=10.0)) for i in range(ndim)]
+
+    # Set the active point population size
+    population_size = 100
+    # Setup the Nested Sampling run
+    n_params = len(sampled_parameters)
+    print("Sampling a total of {} parameters".format(n_params))
+    #population_size = 10
+    print("Will use NS population size of {}".format(population_size))
+    # Construct the Nested Sampler
+    MNNS = MultiNestNestedSampling(sampled_parameters=sampled_parameters,
+                                   loglikelihood=loglikelihood,
+                                   population_size=population_size)
+    #print(PCNS.likelihood(np.array([1.0])))
+    #quit()
+    # run it
+    log_evidence, log_evidence_error = MNNS.run(verbose=True)
+    # Print the output
+    print("log_evidence: {} +- {} ".format(log_evidence, log_evidence_error))
+    print("analytic log_evidence: {}".format(analytic_log_evidence(ndim, width)))
+    #try plotting a marginal distribution
+    try:
+        import seaborn as sns
+        import matplotlib.pyplot as plt
+        # Get the posterior distributions -- the posteriors are return as dictionary
+        # keyed to the names of the sampled paramters. Each element is a histogram
+        # estimate of the marginal distribution, including the heights and centers.
+        posteriors = MNNS.posteriors()
+        # Lets look at the first paramter
+        marginal, centers = posteriors[list(posteriors.keys())[0]]
+        # Plot with seaborn
+        sns.distplot(centers, bins=centers, hist_kws={'weights':marginal})
+        # Uncomment next line to plot with plt.hist:
+        # plt.hist(centers, bins=centers, weights=marginal)
+        plt.show()
+    except ImportError:
+        pass
