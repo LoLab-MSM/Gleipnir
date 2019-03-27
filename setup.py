fbb9e5954cc6c5e0147885c1287ef5239b19a31e
Blake <blakeaw1102@gmail.com>
2019-03-27 15:55:38 -0500

    Added the environment.yml for conda. Added the setup.py for distutils installation. Corresponding changes to README.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='gleipnir',
+      version='0.1.0',
+      description='Python toolkit for Nested Sampling.',
+      author='Blake A. Wilson',
+      author_email='blake.a.wilson@vanderbilt.edu',
+      url='https://github.com/LoLab-VU/Gleipnir',
+      packages=['gleipnir', 'gleipnir.pysb_utilities'],
+     )
