69dcba097636b3ae3bbb983c6a5aad2a17e38c99
Blake <blakeaw1102@gmail.com>
2019-04-18 14:18:22 -0500

    Added try except import guard for HypSelector and ModelSelector inports with a warning in the except statement that HypBuilder is needed for those classes.

--- gleipnir/pysb_utilities/__init__.py
+++ gleipnir/pysb_utilities/__init__.py
@@ -1,3 +1,7 @@
+import warnings
 from .nestedsample_it import NestedSampleIt
-from .hyp_selector import HypSelector
-from .model_selector import ModelSelector
+try:
+    from .hyp_selector import HypSelector
+    from .model_selector import ModelSelector
+except:
+    warnings.warn('Unable to load HypSelector and ModelSelector into pysb_utilities. If you want to use these tools please install HypBuilder.')
