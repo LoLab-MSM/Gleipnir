aa4aa530a22450513c2a5b39c24633fd9f21d41d
Blake <blakeaw1102@gmail.com>
2019-05-29 17:50:20 -0500

    Bump the PATCH version number.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='gleipnir',
-      version='0.22.0',
+      version='0.22.1',
       description='Python toolkit for Nested Sampling.',
       author='Blake A. Wilson',
       author_email='blake.a.wilson@vanderbilt.edu',
